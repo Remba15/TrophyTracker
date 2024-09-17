d3478cc1ea0958d234120d7e742b1966632e13a2
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-09-17 21:05:51 +0200

    Added GameController

---
+++ TrophyTracker/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrophyTracker.Data;
+
+namespace TrophyTracker.Controllers
+{
+    [ApiController]
+    [Route("Api/v1/[controller]")]
+    public class GameController : ControllerBase
+    {
+
+        private readonly TrophyTrackerContext _context;
+
+        public GameController(TrophyTrackerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(_context.Games);
+
+        }
+
+    }
+}
