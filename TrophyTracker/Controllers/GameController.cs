db1dc4a4343de894bce0a022bd3963e316bc7e8c
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-09-19 13:02:32 +0200

    Added GameController

--- TrophyTracker/Controllers/GameController.cs
+++ TrophyTracker/Controllers/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TrophyTracker.Data;
+using TrophyTracker.Models;
 
 namespace TrophyTracker.Controllers
 {
@@ -22,6 +23,53 @@ namespace TrophyTracker.Controllers
             return Ok(_context.Games);
 
         }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_context.Games.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Game game)
+        {
+            _context.Games.Add(game);
+            _context.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created, game);
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        [Produces("application/json")]
+        public IActionResult Put(int id, Game game)
+        {
+
+            var gameDb = _context.Games.Find(id);
+
+            gameDb.Title = game.Title;
+            gameDb.GameDescription = game.GameDescription;
+            gameDb.GamePlatform = game.GamePlatform;
+
+            _context.Games.Update(gameDb);
+            _context.SaveChanges();
+
+            return Ok(new { message = "Successfully updated" });
+
+        }
+
+        //[HttpDelete]
+        //[Route("{id:int}")]
+        //[Produces("application/json")]
+        //public IActionResult Delete(int id)
+        //{
+        //    var gameDb = _context.Games.Find(id);
+
+        //    _context.Games.Remove(gameDb);
+        //    _context.SaveChanges();
+
+        //    return Ok(new { message = "Successfully removed" });
+        //}
 
     }
 }
