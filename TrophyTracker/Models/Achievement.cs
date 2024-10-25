d0b2955769b26fca1f99ceff208a1a24c19d9b1d
Remba15 <60290044+Remba15@users.noreply.github.com>
2024-10-25 18:10:21 +0200

    Added Achievement model

---
+++ TrophyTracker/Models/Achievement.cs
@@ -0,0 +1,11 @@
+﻿namespace TrophyTracker.Models
+{
+    public class Achievement
+    {
+
+        public int Player_ID { get; set; }
+        public int Trophy_ID { get; set; }
+        public DateTime? DateAchieved { get; set; }
+
+    }
+}
